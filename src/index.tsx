7a4902db6d519b1525c94bce4244167011d9f258
Junwon Park <julrumg@gmail.com>
2021-08-19 17:38:57 +0900

    developer: Apply Global theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import theme from './lib/styles/theme';
+import GlobalStyle from './lib/styles/globalStyle';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
 
---
+++ src/lib/styles/styled.d.ts
@@ -0,0 +1,33 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    fontSizes: {
+      header1: string;
+      header2: string;
+      body: string;
+    };
+
+    colors: {
+      bold: string;
+      main: string;
+      sub: string;
+      placeholder: string;
+      white: string;
+      background: string;
+      bgDivider: string;
+      disabledbg: string;
+      disabledborder: string;
+      primary100: string;
+      primary80: string;
+      primary60: string;
+      primary40: string;
+      primary20: string;
+      secondary100: string;
+      secondary80: string;
+      danger1: string;
+      danger2: string;
+      warning: string;
+    };
+  }
+}
