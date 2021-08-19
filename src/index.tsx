fff06b0cc997fec73ed4fa6a0741efc2377c3b09
Junwon Park <julrumg@gmail.com>
2021-08-19 23:10:15 +0900

    enhancement: Change favicon and title

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import theme from './lib/styles/theme';
 import GlobalStyle from './lib/styles/globalStyle';
 
@@ -17,8 +15,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
