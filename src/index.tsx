3191d9ee44fccc4306852d3175197ac8e3be979d
Junwon Park <julrumg@gmail.com>
2021-08-21 18:24:33 +0900

    enhancement: Apply fonts

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import Routes from '@pages/Routes';
-import theme from './lib/styles/theme';
-import GlobalStyle from './lib/styles/globalStyle';
+import theme from '@styles/theme';
+import GlobalStyle from '@styles/globalStyle';
+import FontStyle from '@styles/fonts/fonts';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
+    <FontStyle />
     <GlobalStyle />
     <Routes />
   </ThemeProvider>,
