dd044c26bf9df06b3cf7f788661a37d5d1d8640e
Junwon Park <julrumg@gmail.com>
2021-08-22 15:36:58 +0900

    bug: fix contents align center

--- src/components/templates/BasicLayout/BasicLayout.tsx
+++ src/components/templates/BasicLayout/BasicLayout.tsx
@@ -3,16 +3,23 @@ import { Route, RouteComponentProps } from 'react-router-dom';
 import { MatchParams } from '@common/router.type';
 import MenuBar from '@components/organisms/MenuBar';
 import Home from '@pages/Home';
-import { Wrapper, Content, MenuBarSpace } from './BasicLayout.style';
+import {
+  Wrapper,
+  Content,
+  ContentWrapper,
+  MenuBarSpace,
+} from './BasicLayout.style';
 
 const BasicLayout: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => {
   return (
     <Wrapper>
       <MenuBar />
       <MenuBarSpace />
-      <Content>
-        <Route exact path={match.url} component={Home} />
-      </Content>
+      <ContentWrapper>
+        <Content>
+          <Route exact path={match.url} component={Home} />
+        </Content>
+      </ContentWrapper>
     </Wrapper>
   );
 };
