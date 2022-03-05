4ad8f79afec5480efb4b10525389c77913d0d211
Junwon Park <julrumg@gmail.com>
2022-03-05 14:24:51 +0900

    enhancement: Update About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import Github from 'components/atoms/Icons/Github';
+import Linkedin from 'components/atoms/Icons/Linkedin';
 import Layout from 'components/templates/Layout';
 
 const About = () => {
@@ -10,19 +13,29 @@ const About = () => {
           <strong>junior software engineer</strong> located in Republic of
           Korea.
         </p>
-        <p>I use julrum as my primary username in various sites.</p>
+        <p>I am working on several projects to become a front-end developer.</p>
         <p>
-          I often program using <strong>C/C++(C++17)</strong>,{' '}
-          <strong>Python</strong> and <strong>React</strong>. I am currently a
-          college student. I&rsquo;m writing what I learned on this blog.
+          I often program using <strong>Typescript</strong>,{' '}
+          <strong>React</strong>, <strong>Next.JS</strong>,{' '}
+          <strong>C/C++(C++17)</strong> and <strong>React-Native</strong>. I am
+          currently a college student. I&rsquo;m writing what I learned on this
+          blog.
         </p>
         <h3 id="social-links">Social Links</h3>
         <ul>
           <li>
-            <a href="https://github.com/Julrum">GitHub</a>
+            <Link href="https://github.com/Julrum" passHref>
+              <a>
+                <Github />
+              </a>
+            </Link>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/julrum/">Linkedin</a>
+            <Link href="https://www.linkedin.com/in/julrum/" passHref>
+              <a>
+                <Linkedin />
+              </a>
+            </Link>
           </li>
         </ul>
       </div>
