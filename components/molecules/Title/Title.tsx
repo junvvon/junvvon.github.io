895e175281d8e1b0c0703944b8c16dd0c378de90
Julrum <Julrum@gmail.com>
2022-05-03 17:15:45 +0900

    enhancement: Fix Title component for about page

--- components/molecules/Title/Title.tsx
+++ components/molecules/Title/Title.tsx
@@ -2,23 +2,55 @@ import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import { ko } from 'date-fns/locale';
 import Github from 'components/atoms/Icons/Github';
+import Linkedin from 'components/atoms/Icons/Linkedin';
+import Mail from 'components/atoms/Icons/Mail';
 import { Titleprops } from './Title.type';
-import { Header, StyledTitle, Divider } from './Title.style';
+import { Header, StyledTitle, Divider, Icons, Icon } from './Title.style';
 
-const Title = ({ date, link, title }: Titleprops) => {
+const Title = ({
+  date,
+  githubLink,
+  linkedinLink,
+  mailLink,
+  title,
+}: Titleprops) => {
   return (
     <>
       <Header>
         <StyledTitle>{title}</StyledTitle>
-        {link && (
-          <Link href={link} passHref>
-            <a target="_blank">
-              <Github />
-            </a>
-          </Link>
-        )}
+        <Icons>
+          {githubLink && (
+            <Icon>
+              <Link href={githubLink} passHref>
+                <a target="_blank">
+                  <Github />
+                </a>
+              </Link>
+            </Icon>
+          )}
+          {linkedinLink && (
+            <Icon>
+              <Link href={linkedinLink} passHref>
+                <a target="_blank">
+                  <Linkedin />
+                </a>
+              </Link>
+            </Icon>
+          )}
+          {mailLink && (
+            <Icon>
+              <Link href={mailLink} passHref>
+                <a>
+                  <Mail />
+                </a>
+              </Link>
+            </Icon>
+          )}
+        </Icons>
       </Header>
-      <p>{format(parseISO(String(date)), 'MMMM do, yyyy', { locale: ko })}</p>
+      {date && (
+        <p>{format(parseISO(String(date)), 'MMMM do, yyyy', { locale: ko })}</p>
+      )}
       <Divider />
     </>
   );
