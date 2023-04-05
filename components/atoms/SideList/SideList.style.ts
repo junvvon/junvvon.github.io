b9e770f6f2cefeb4b28034b4c6109659417fc5a0
jvnwon <junwon@vaultmicro.com>
2023-04-05 16:43:08 +0900

    refactor: Remove unnecessary code on SideList.style.ts

--- components/atoms/SideList/SideList.style.ts
+++ components/atoms/SideList/SideList.style.ts
@@ -3,9 +3,3 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   margin: 10px;
 `;
-
-export const List = styled.ul`
-  /* margin: 2px 0px;
-  padding-left: 5px;
-  white-space: nowrap; */
-`;
