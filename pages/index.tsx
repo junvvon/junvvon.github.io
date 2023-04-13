4f9185d0a6177b936ebff4a4d30c62997ad88cc4
jvnwon <junwon@vaultmicro.com>
2023-04-13 10:45:29 +0900

    feat: Add firebase on resume page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
 import type { GetStaticProps } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
 import dynamic from 'next/dynamic';
 import { getApps, initializeApp } from 'firebase/app';
-import {
-  DocumentData,
-  collection,
-  getDocs,
-  getFirestore,
-} from 'firebase/firestore/lite';
+import { doc, getDoc, getFirestore } from 'firebase/firestore/lite';
 
 import OutLink from 'components/atoms/OutLink';
 import Title from 'components/molecules/Title';
 import Layout from 'components/templates/Layout';
 import Row from 'components/templates/Row';
 import firebaseConfig from 'config/firebaseConfig';
+import {
+  CertificateProps,
+  SkillProps,
+  WorkExperienceProps,
+  OtherExperienceProps,
+} from 'types/about';
 
 const SideList = dynamic(() => import('components/atoms/SideList'));
 const Project = dynamic(() => import('components/molecules/Project'));
@@ -22,23 +22,16 @@ const SideListLayout = dynamic(
   () => import('components/templates/SideListLayout'),
 );
 
-interface WorkExperienceProps {
-  companyName: string;
-  role: string;
-  dateFrom: string;
-  project: {
-    skill: string;
-    dateFrom: string;
-    title: string;
-    detail: string[];
-    description: string;
-  }[];
-}
-
 const About = ({
-  work_experience,
+  workExperiences,
+  otherExperiences,
+  certificate,
+  skill,
 }: {
-  work_experience: WorkExperienceProps[];
+  workExperiences: WorkExperienceProps[] | null;
+  otherExperiences: OtherExperienceProps[] | null;
+  certificate: { certificates: CertificateProps[] };
+  skill: { skill: SkillProps[] };
 }) => (
   <Layout customMeta={{ title: 'Junwon Park' }}>
     <Head>
@@ -72,222 +65,117 @@ const About = ({
         </div>
         <h2>Work Experience</h2>
 
-        {work_experience.map((work, index: number) => (
-          <Row
-            key={index}
-            companyName={work.companyName}
-            role={work.role}
-            dateFrom={work.dateFrom}
-          >
-            {work.project.map((singleProject, index: number) => (
-              <Project
-                key={index}
-                description={<p>{singleProject.description}</p>}
-                skill={singleProject.skill}
-                dateFrom={singleProject.dateFrom}
-                title={singleProject.title}
-              >
-                <ul>
-                  {singleProject.detail.map((line: string, index: number) => (
-                    <li key={index}>{line}</li>
-                  ))}
-                </ul>
-              </Project>
-            ))}
-          </Row>
-        ))}
-
-        <Row
-          additional={
-            <OutLink
-              label="추천서 보기"
-              link="https://drive.google.com/file/d/16fqD52QfKeZN4-QQokJx-mFzAiulpEZu/view?usp=sharing"
-            />
-          }
-          companyName="대한민국 국방부"
-          role="SW 개발병"
-          dateFrom="2019-05"
-          dateTo="2020-12"
-        >
-          <Project
-            description={
-              <p>
-                워드와 엑셀 파일을 웹 내에서 특정 라인 및 셀 강조 표시, 커멘트
-                기능이 있는 웹페이지 제작 프로젝트입니다. 프로젝트 기획, 디자인,
-                개발 전 과정에 참여하였습니다. Antd를 사용했던 이전 프로젝트와
-                달리 모든 컴포넌트를 직접 디자인 개발하였습니다.
-              </p>
-            }
-            skill="Typescript, React, Redux, styled-components, React Router"
-            dateFrom="2020-08"
-            dateTo="2020-12"
-            title="워드, 엑셀 웹 뷰어"
-          >
-            <ul>
-              <li>프론트엔드 기술환경 구성 및 핵심 기능 구현</li>
-              <li>Redux, Redux-Saga를 사용한 스토어 디자인</li>
-              <li>React기반 컴포넌트 계층 구조 디자인</li>
-              <li>Atomic 디자인</li>
-              <li>Lazy loading을 사용한 뷰어 최적화</li>
-            </ul>
-          </Project>
-          <Project
-            description={
-              <p>
-                국방부 망 내에서 사용 가능한 대시보드 웹페이지 제작
-                프로젝트입니다. 프로젝트 중간부터 참여하였습니다. 유지보수 및
-                아직 개발되지 않은 새로운 기능을 개발하였습니다.
-              </p>
-            }
-            skill="Javascript, React, Antd, Redux, styled-components, React Router"
-            dateFrom="2019-06"
-            dateTo="2019-09"
-            title="대시보드 페이지"
-          >
-            <ul>
-              <li>
-                테이블 컴포넌트가 사용된 페이지의 성능 개선 및 정렬 기능 구현
-              </li>
-              <li>Ant Design 라이브러리를 활용하여 컴포넌트를 사용 및 개조</li>
-            </ul>
-          </Project>
-        </Row>
+        {workExperiences &&
+          workExperiences.map((experience, index: number) => (
+            <Row
+              additional={
+                experience.additional && (
+                  <OutLink
+                    label={experience.additional?.label}
+                    link={experience.additional?.link}
+                  />
+                )
+              }
+              key={index}
+              companyName={experience.companyName}
+              role={experience.role}
+              dateFrom={experience.dateFrom}
+              dateTo={experience.dateTo}
+            >
+              {experience.project.map((singleProject, index: number) => (
+                <Project
+                  dateFrom={singleProject.dateFrom}
+                  dateTo={singleProject.dateTo}
+                  description={singleProject.description}
+                  detailLink={singleProject.detailLink}
+                  key={index}
+                  skill={singleProject.skill}
+                  title={singleProject.title}
+                >
+                  <ul>
+                    {singleProject.detail.map((line: string, index: number) => (
+                      <li key={index}>{line}</li>
+                    ))}
+                  </ul>
+                </Project>
+              ))}
+            </Row>
+          ))}
 
         <h2>Other Experiences</h2>
-        <Row
-          companyName="한양대학교"
-          dateFrom="2017-03"
-          role="컴퓨터소프트웨어학부 전공"
-        >
-          <Project
-            description={
-              <p>
-                한양대학교 소프트웨어스튜디오2 수업 과제로 대선 관련 정보를 모아
-                볼 수 있는 웹페이지를 제작 프로젝트 입니다. 팀원 4명이서
-                진행하였으며 팀장을 맡아 기획, 디자인 개발 등 전 과정을
-                담당하였습니다. <Link href="/posts/daesun">자세히 보기</Link>
-              </p>
-            }
-            githubLink="https://github.com/Julrum/ITE3068_team8"
-            skill="Javascript, React, AWS Amplify, GraphQL, styled-components"
-            dateFrom="2021-11"
-            dateTo="2021-12"
-            title="대선 뉴스 모아보기 페이지"
-          >
-            <ul>
-              <li>프론트엔드 기술환경 구성 및 기본 기능 구현</li>
-              <li>
-                AWS Amplify를 이용한 회원 정보 관리 및 로그인, 로그아웃,
-                회원가입 기능 구현
-              </li>
-              <li>GraphQL을 이용한 데이터 관리</li>
-              <li>React기반 컴포넌트 계층 구조 디자인</li>
-            </ul>
-          </Project>
-        </Row>
-        <Project
-          description={
-            <p>
-              2021 우리은행 온(On)택트 해커톤 본선대회에 참여하여 개발한
-              프로젝트입니다. 복잡해진 은행 앱의 사용 편의성을 위하여 채팅을
-              통한 송금 기능을 가진{' '}
-              <OutLink
-                label="어플리케이션"
-                link="https://github.com/Julrum/OnelineBank/releases/tag/v1.0"
-              />
-              을 개발하였습니다. 대회에는 팀원과 같이 나갔으나 개발은 100%
-              혼자서 진행하였습니다. React와 달리 React Native는 경험이 없어
-              책으로 학습 후 개발 완료까지 5일이 소요되었습니다.{' '}
-              <Link href="/posts/onelinebank">자세히 보기</Link>
-            </p>
-          }
-          githubLink="https://github.com/Julrum/OnelineBank"
-          other
-          skill="Javascript, React Native, Firebase, Expo"
-          dateFrom="2021-04"
-          dateTo="2021-06"
-          title="한줄 은행"
-        >
-          <ul>
-            <li>프론트엔드 기술환경 구성 및 모든 기능 구현</li>
-            <li>Firebase를 사용한 스토어 디자인</li>
-            <li>우리 은행 API를 사용한 송금 기능 구현</li>
-            <li>생체인증을 이용한 사용자 확인 기능 구현</li>
-          </ul>
-        </Project>
-        <Project
-          description={
-            <p>
-              Next.js를 이용하여{' '}
-              <OutLink label="이력서" link="https://julrum.github.io" />를
-              제작하는 프로젝트입니다. 처음엔 React로 개발하였으나 pre-rendered
-              정적 페이지 구현을 위해 Next.js로 변경하였습니다.{' '}
-            </p>
-          }
-          githubLink="https://github.com/Julrum/julrum.github.io"
-          other
-          skill="Typescript, Next.js, styled-components, MDX"
-          dateFrom="2021-02"
-          title="개인 블로그"
-        >
-          <ul>
-            <li>프론트엔드 기술환경 구성 및 모든 기능 구현</li>
-            <li>Atomic 디자인</li>
-            <li>웹브라우저의 테마에 따른 테마 변경 기능</li>
-            <li>다양한 브라우저 환경 및 사이즈 대응</li>
-          </ul>
-        </Project>
+        {otherExperiences &&
+          otherExperiences.map((experience, index) =>
+            experience.companyName && experience.dateFrom && experience.role ? (
+              <Row
+                companyName={experience.companyName}
+                dateFrom={experience.dateFrom}
+                role={experience.role}
+                key={index}
+              >
+                {experience.project.map((singleProject, index: number) => (
+                  <Project
+                    dateFrom={singleProject.dateFrom}
+                    dateTo={singleProject.dateTo}
+                    description={singleProject.description}
+                    detailLink={singleProject.detailLink}
+                    githubLink={singleProject.gitthuLink}
+                    key={index}
+                    skill={singleProject.skill}
+                    title={singleProject.title}
+                  >
+                    <ul>
+                      {singleProject.detail.map(
+                        (line: string, index: number) => (
+                          <li key={index}>{line}</li>
+                        ),
+                      )}
+                    </ul>
+                  </Project>
+                ))}
+              </Row>
+            ) : (
+              <>
+                {experience.project.map((singleProject, index: number) => (
+                  <Project
+                    dateFrom={singleProject.dateFrom}
+                    dateTo={singleProject.dateTo}
+                    description={singleProject.description}
+                    detailLink={singleProject.detailLink}
+                    githubLink={singleProject.gitthuLink}
+                    key={index}
+                    other
+                    skill={singleProject.skill}
+                    title={singleProject.title}
+                  >
+                    <ul>
+                      {singleProject.detail.map(
+                        (line: string, index: number) => (
+                          <li key={index}>{line}</li>
+                        ),
+                      )}
+                    </ul>
+                  </Project>
+                ))}
+              </>
+            ),
+          )}
 
         <h2>Certificates</h2>
         <ul>
-          <li>
-            <OutLink
-              label="AWS training and certification"
-              link="https://drive.google.com/file/d/1LzNVPiGx2hFrqHJdnqedG1TrOatShHK-/view?usp=sharing"
-            />
-          </li>
-          <li>
-            <OutLink
-              label="TOPCIT"
-              link="https://drive.google.com/file/d/1Pl_UzJ8mkrCNYbl3HPsu9gbzctfQlPK1/view?usp=sharing"
-            />
-          </li>
-          <li>
-            <OutLink
-              label="리눅스 마스터 2급"
-              link="https://drive.google.com/file/d/1vzBdsUZke4CE7rVAzCgNvlEOEmpQWgm7/view?usp=sharing"
-            />
-          </li>
+          {certificate &&
+            certificate.certificates.map((certificate, index: number) => (
+              <li key={index}>
+                <OutLink label={certificate.label} link={certificate.link} />
+              </li>
+            ))}
         </ul>
 
         <h2>SKILLS</h2>
         <SideListLayout>
-          <SideList
-            title="Front-End"
-            list={[
-              'React',
-              'Next.js',
-              'JavaScript',
-              'TypeScript',
-              'HTML5',
-              'CSS3',
-            ]}
-          />
-          <SideList
-            title="Back-End"
-            list={['Firebase', 'GCP', 'AWS', 'GraphQL', 'Java', 'Python']}
-          />
-          <SideList title="Application" list={['React Native']} />
-          <SideList
-            title="etc"
-            list={[
-              'Ubuntu',
-              'Git/Github/Gitlab',
-              'Confluence',
-              'JIRA',
-              'Figma',
-            ]}
-          />
+          {skill &&
+            skill.skill.map((skill, index: number) => (
+              <SideList key={index} title={skill.title} list={skill.list} />
+            ))}
         </SideListLayout>
       </div>
     </article>
@@ -295,20 +183,31 @@ const About = ({
 );
 
 export const getStaticProps: GetStaticProps = async () => {
-  let work_experience: DocumentData[] = [];
   if (!getApps().length) {
     initializeApp(firebaseConfig);
   }
 
   const db = getFirestore();
-  const querySnapshot = await getDocs(collection(db, 'work_experience'));
-  querySnapshot.forEach((doc) => {
-    work_experience.push(doc.data());
-  });
+
+  const workExperiences = await getDoc(
+    doc(db, 'about', 'work_experience'),
+  ).then((doc) => doc.data());
+  const otherExperiences = await getDoc(
+    doc(db, 'about', 'other_experience'),
+  ).then((doc) => doc.data());
+  const certificate = await getDoc(doc(db, 'about', 'certificates')).then(
+    (doc) => doc.data(),
+  );
+  const skill = await getDoc(doc(db, 'about', 'skills')).then((doc) =>
+    doc.data(),
+  );
 
   return {
     props: {
-      work_experience: work_experience.length === 0 ? null : work_experience,
+      workExperiences: workExperiences?.work_experience,
+      otherExperiences: otherExperiences?.other_experience,
+      certificate,
+      skill,
     },
   };
 };
